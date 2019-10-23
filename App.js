98307c0a2ee772c98c37767a1fe868211b54fa0d
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-10-23 20:57:09 -0300

    config editorConfig, Eslint, Prettier

--- App.js
+++ App.js
@@ -1,19 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <View />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
