7f4ee3468cd42cdfd0f0be0d9ca92333d643a621
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-10-25 15:23:14 -0300

    add component Background, add linear gradient

---
+++ src/components/Background/index.js
@@ -0,0 +1,8 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import styled from 'styled-components/native';
+
+export default styled(LinearGradient).attrs({
+  colors: ['#7159c1', '#ab59c1'],
+})`
+  flex: 1;
+`;
