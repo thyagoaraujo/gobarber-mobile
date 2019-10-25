429ebc7347faf60a8364ec555177de40e9395e12
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-10-25 16:46:39 -0300

    add components Input and Button, propTypes

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { YellowBox } from 'react-native';
 
 import Routes from './src/routes';
+
+YellowBox.ignoreWarnings([
+  'Warning: forwardRef render functions do not support',
+]);
 
 export default function App() {
   return <Routes />;
