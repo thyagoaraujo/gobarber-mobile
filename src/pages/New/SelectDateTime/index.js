70214946fc0baf1d4870db5ed1efb0f3b198f717
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-11-09 13:49:54 -0300

    add DatePickerIOS

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '../../../components/Background';
+import DateInput from '../../../components/DateInput';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function SelectDateTime() {
-  return <Background />;
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <Background>
+      <Container>
+        <DateInput date={date} onChange={setDate} />
+      </Container>
+    </Background>
+  );
 }
 
 SelectDateTime.navigationOptions = ({ navigation }) => ({
